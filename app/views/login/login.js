d288e3866b752495ff6938f29ab50f7fe743dc49
Joabe Costa <joabetc@hotmail.com>
2018-03-04 15:32:36 -0300

    Adding a navigational change while tapping register button

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,3 +1,5 @@
+var frameModule = require("ui/frame");
+
 exports.loaded = function() {
     console.log("hello");
 };
@@ -7,5 +9,6 @@ exports.signIn = function() {
 };
 
 exports.register = function() {
-    alert("Registering");
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
 };
