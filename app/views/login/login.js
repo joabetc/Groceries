3b629e510c22411d2189cf430aaa738c9ff2dfc0
Joabe Costa <joabetc@hotmail.com>
2018-03-07 19:17:19 -0300

    Creating a view model and bind it to the view

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,9 +1,16 @@
 var frameModule = require("ui/frame");
+var observableModule = require("data/observable");
+
+var user = new observableModule.fromObject({
+    email: "user@domain.com",
+    password: "password"
+});
+
 var page;
-var email;
 
 exports.loaded = function(args) {
     page = args.object;
+    page.bindingContext = user;
 };
 
 exports.signIn = function() {
