b6c617199f9df75c724deeb25e443b89f503268b
Joabe Costa <joabetc@hotmail.com>
2018-03-10 16:29:16 -0300

    Handling an error with a dialog window

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,6 +1,7 @@
 var frameModule = require("ui/frame");
 var observableModule = require("data/observable");
 var UserViewModel = require("../../shared/view-models/user-view-model");
+var dialogsModule = require("ui/dialogs");
 
 var user = new UserViewModel();
 
@@ -12,7 +13,18 @@ exports.loaded = function(args) {
 };
 
 exports.signIn = function() {
-    user.login();
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        })
 };
 
 exports.register = function() {
